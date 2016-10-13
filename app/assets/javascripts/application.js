e2e5840f854ce5cb188fccac552a4d73e6d48ae8
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-10-13 20:17:54 +0700

    fix i18n

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).on('ready', function() {
+  setTimeout(function() {
+    $('.alert').fadeOut('slow');
+  }, 3000);
+});
