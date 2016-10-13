b437f123114381e1b4fd82075c2e8c21cadbabbb
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-10-13 09:50:26 +0700

    authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :rememberable, :validatable
   has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile, allow_destroy: true
 
   has_many :post, dependent: :destroy
   has_many :evaluation, dependent: :destroy
   has_many :comment, dependent: :destroy
+
+  enum gender: [:male, :female]
+
+  def password_required?
+    new_record? ? super : false
+  end
 end
